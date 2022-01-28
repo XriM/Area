e97c553657464eee68069bf42cb7679a5df9d0cb
Jefferson Guiot <jefferson.guiot@epitech.eu>
2022-01-28 20:13:34 +0100

    fix: server working with database

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const cors = require("cors");
+const pool = require("./appConfig");
+const bcrypt = require('bcrypt');
 
 // Constants
 const PORT = 8000;
@@ -9,11 +11,11 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-const allowedOrigins = ['http://localhost:3000'];
 app.use(cors({
-  origin: allowedOrigins,
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -21,3 +23,56 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+
+app.get('/users/signup', (req, res) => {
+    res.render('signup')
+    console.log("ICI:")
+    console.log(process.env.DB_NAME)
+});
+app.post('/users/signup', (req, res) => {
+  console.log(req.body);
+  let email = req.body.email;
+  let password = req.body.password;
+  let username = req.body.email;
+
+  console.log({
+      email,
+      username,
+      password
+  });
+
+  // let errors = [];
+
+  // if (!email || !username || !password) {
+  //     errors.push({message: "Please enter all fields"});
+  // }
+
+  // if (password.length < 6) {
+  //     errors.push({message: "Password should be at least 6 characters"});
+  // }
+
+  // if (errors.length > 0) {
+  //     res.render('signup', { errors })
+  // } else {
+  //     let hashedPassword = await bcrypt.hash(password, 10);
+  //     console.log(hashedPassword)
+
+  //     pool.query(
+  //         `SELECT * FROM user_table
+  //         WHERE email = $1`,
+  //         [email],
+  //         (err, results) => {
+  //             if (err) {
+  //                 throw err;
+  //             }
+  //             console.log(results.rows);
+
+  //             if (results.row.length > 0) {
+  //                 errors.push({message: "Email already exist"})
+  //                 res.render('signup', { errors })
+  //             }
+  //         }
+  //     )
+  // }
+});
