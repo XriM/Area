ff34616d6c8cf5cb45270b9090715b75a410ce35
Jefferson Guiot <jefferson.guiot@epitech.eu>
2022-02-01 14:48:59 +0100

    fix: env

--- app/token.js
+++ app/token.js
@@ -5,7 +5,7 @@ dotenv.config()
 var token_secret = 'b1be96e1c1b5f8289035813256d4532502224f6058e3a69c94cca2b8102dbb5bf8c2bf7b650b8cc83bdc34829551a4657b4e1d90f8ace32d88b99a9545cfd0ab';
 
 exports.generateAccessToken = (username) => {
-    return jwt.sign({ username: username }, token_secret, { expiresIn: '1h' });
+    return jwt.sign({ username: username }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
 }
 
 exports.authenticateToken = (req, res, next) => {
@@ -14,7 +14,7 @@ exports.authenticateToken = (req, res, next) => {
 
     if (token == null) return res.status(401).send({ message: 'You need to signin to an account!' });
 
-    jwt.verify(token, token_secret, (err, user) => {
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
         console.log(user);
         if (err) {
             return res.status(498).send({ message: 'Invalid token!' });
--- dbConfig.js
+++ dbConfig.js
@@ -1,9 +1,11 @@
+require("dotenv").config()
+
 const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'root',
-  host: 'db',
-  database: 'area',
-  password: 'dbpass',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
   port: 5432,
 })
 
