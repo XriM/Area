520942e597615367104a6906cb0d64b3b1f8f0fd
David NIKOLIC <david.nikolic@epitech.eu>
2022-02-01 14:47:05 +0100

    .feat adding second screen

--- flutter_docker/workspace/lib/routes/second_screen.dart
+++ flutter_docker/workspace/lib/routes/second_screen.dart
@@ -5,10 +5,50 @@ class SecondScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Second Screen"),
-      ),
-    );
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+            height: 60.0,
+            width: 300.0,
+            color: Colors.transparent,
+            child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.lightBlue,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                child: Container(
+                  margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                  child: const SizedBox(
+                    width: 280,
+                    child: Text(
+                      "IF THIS",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ))),
+        Container(
+            height: 60.0,
+            width: 300.0,
+            color: Colors.transparent,
+            child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.lightBlue,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                child: Container(
+                  margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                  child: const SizedBox(
+                    width: 280,
+                    child: Text(
+                      "IF THIS",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                )))
+      ],
+    ));
   }
 }
