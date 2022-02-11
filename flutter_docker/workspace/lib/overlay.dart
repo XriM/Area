72eb1c5a520bc2d50fd76d9ddef7dea132b8c607
David NIKOLIC <david.nikolic@epitech.eu>
2022-02-11 12:13:31 +0100

    .feat adding overlay and loader

---
+++ flutter_docker/workspace/lib/overlay.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'loader.dart';
+
+class OverlayView extends StatelessWidget {
+  OverlayView({
+    Key? key,
+  }) : super(key: key);
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: Loader.appLoader.loaderShowingNotifier,
+      builder: (context, value, child) {
+        if (value) {
+          return yourOverLayWidget();
+        } else {
+          return Container();
+        }
+      },
+    );
+  }
+
+  Scaffold yourOverLayWidget() {
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'AREA',
+                      style: TextStyle(
+                          color: Color(0xff333333),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    //forgot password screen
+                  },
+                  child: const Text(
+                    'Forgot Password',
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      child: const Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                          primary: const Color(0xff333333)),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                        Loader.appLoader.hideLoader();
+                      },
+                    )),
+                Row(
+                  children: <Widget>[
+                    const Text('Does not have account?'),
+                    TextButton(
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        //signup screen
+                      },
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+              ],
+            )));
+  }
+}
