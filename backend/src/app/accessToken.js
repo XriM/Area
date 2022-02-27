213aefbb7a4316a973ae2d4e045fe3dc80f12696
Maxime <maxime.sarres@epitech.eu>
2022-02-27 21:17:21 +0100

    feat: GitHub handler for token

--- backend/src/app/accessToken.js
+++ backend/src/app/accessToken.js
@@ -4,11 +4,8 @@ const { postUserService } = require('./services')
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
 
-exports.accessTokensHandler = async (req, res) => {
-    const body = req.body
-
-    //if ('github' in body) {
-        const CODE = body.code
+exports.accessTokenGitHub = async (oldToken) => {
+        const CODE = oldToken
 
         const githubToken = await axios.get(`https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${CODE}`).then((res) => res.data).catch((error) => {
             throw error
@@ -21,8 +18,5 @@ exports.accessTokensHandler = async (req, res) => {
             console.error('Error getting user from GitHub')
             throw error
         })
-        req.body.code = accessToken
-        postUserService(req, res)
-        res.status(200).send({ message: 'Service token successfully loaded' })
-    //}
+        return accessToken
 }
