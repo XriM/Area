27127cd8f6569a1dcbe4742fa65fb9fd46b92738
David NIKOLIC <david.nikolic@epitech.eu>
2022-03-02 17:43:16 +0100

    .feat fixing rename folder

--- flutter_docker/workspace/android/app/src/main/kotlin/com/example/workspace/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.workspace
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_docker/workspace/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFF333333);
-const kPrimaryLightColor = Color(0xFFE4E4E4);
